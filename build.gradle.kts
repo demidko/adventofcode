fcc9c076123ef3e8be8737e6160fe0da8da45f20
Daniil Demidko <demidko@farpost.com>
2021-12-02 04:18:00 +1000

    Тесты нам не пригодятся

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,19 +6,11 @@ plugins {
   kotlin("jvm") version "1.6.0"
   id("com.github.johnrengelman.shadow") version "7.1.0"
 }
-dependencies {
-  testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-  testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
-  testImplementation("io.mockk:mockk:1.12.0")
-}
 tasks.compileKotlin {
   kotlinOptions.jvmTarget = "17"
 }
 tasks.compileTestKotlin {
   kotlinOptions.jvmTarget = "17"
-}
-tasks.test {
-  useJUnitPlatform()
 }
 tasks.jar {
   isZip64 = true
