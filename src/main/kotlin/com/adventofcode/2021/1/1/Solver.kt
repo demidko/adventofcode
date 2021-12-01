ce90e5592b80926af1e0d44d1d8fa30bd75364f5
Daniil Demidko <demidko@farpost.com>
2021-12-02 04:31:57 +1000

    Хорошенькие имена для классов

--- src/main/kotlin/com/adventofcode/2021/1/1/Solver.kt
+++ src/main/kotlin/com/adventofcode/2021/1/1/Solver.kt
@@ -3,7 +3,7 @@ package com.adventofcode.`2021`.`1`.`1`
 import java.util.concurrent.atomic.AtomicReference
 
 fun print() {
-  val resource = "/com/adventofcode/2021/1.day"
+  val resource = "/com/adventofcode/2021/1/1.part"
   val sonar = AtomicReference<Sonar>()
   Sonar::class.java.getResourceAsStream(resource)!!.reader().forEachLine {
     sonar.get().next(it).let(sonar::set)
