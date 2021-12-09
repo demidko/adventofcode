7ff0c3b9345e1110a3fe4db424a1359aa0f3ab6b
Daniil Demidko <demidko@farpost.com>
2021-12-10 00:54:29 +1000

    Скоро победим

--- src/main/kotlin/com/adventofcode/year2021/day3/part2/App.kt
+++ src/main/kotlin/com/adventofcode/year2021/day3/part2/App.kt
@@ -1,6 +1,7 @@
 package com.adventofcode.year2021.day3.part2
 
 import com.adventofcode.year2021.day
+import java.math.BigInteger
 
 fun List<String>.mostCommonBit(column: Int): Char {
   return when (count { it[column] == '1' } >= count { it[column] == '0' }) {
@@ -16,12 +17,12 @@ fun List<String>.leastCommonBit(column: Int): Char {
   }
 }
 
-tailrec fun List<String>.filterBy(detectBitCriteria: List<String>.(Int) -> Char, column: Int = 0): String {
+tailrec fun List<String>.filterBy(detectBitCriteria: List<String>.(Int) -> Char, column: Int = 0): BigInteger {
   val bitCriteria = detectBitCriteria(column)
   val foundedNumbers = filter { it[column] == bitCriteria }
   return when (foundedNumbers.size > 1) {
     true -> foundedNumbers.filterBy(detectBitCriteria, column + 1)
-    else -> foundedNumbers.first()
+    else -> BigInteger(foundedNumbers.first(), 2)
   }
 }
 
@@ -30,6 +31,5 @@ fun main() {
   val allBinaryNumbers = day(3)
   val oxygenGeneratorRating = allBinaryNumbers.filterBy(List<String>::mostCommonBit)
   val co2ScrubberRating = allBinaryNumbers.filterBy(List<String>::leastCommonBit)
-  println(oxygenGeneratorRating)
-  println(co2ScrubberRating)
+  println(oxygenGeneratorRating * co2ScrubberRating)
 }
